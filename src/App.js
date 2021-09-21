a51525a2f7baf40d23fc98e55cd630254c123673
Warren Williams <warrenw239@gmail.com>
2021-09-21 19:51:01 -0400

    [test] routing after log in

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import NavBarOut from './components/NavBarOut';
 import { Switch, BrowserRouter as Router, Link, Route } from 'react-router-dom';
@@ -8,12 +8,46 @@ import LandingPage from './Pages/LandingPage';
 import ForConsumers from './Pages/ForConsumers';
 import ForContractors from './Pages/ForContractors';
 
-
 function App() {
   const { isAuthenticated, isLoading, user } = useAuth0();
 
+  const testDisplay = <div>hello test</div>;
+  const testDisplay2 = <div>hello test secondary!!</div>;
+
+  const [display, setDisplay] = useState(testDisplay);
+
+  // useEffect(() => {
+  //   // testDisplay;
+  //   if (!isAuthenticated) {
+  //     return (
+  //       <Router>
+  //         <Switch>
+  //           <Route exact path="/">
+  //             <LandingPage />
+  //           </Route>
+  //           <Route exact path="/ForConsumers">
+  //             <ForConsumers />
+  //           </Route>
+  //           <Route exact path="/ForContractors">
+  //             <ForContractors />
+  //           </Route>
+  //         </Switch>
+  //       </Router>
+  //     );
+  //   }
+  //   return () => {
+  //     setDisplay(null);
+  //   };
+  // }, [display]);
+
   if (isLoading) {
-    return <div>loading....</div>;
+    // setDisplay(<NavBarIn />);
+    return (
+      <div>
+        loading....
+        <NavBarIn />
+      </div>
+    );
   }
   if (!isAuthenticated) {
     return (
@@ -26,20 +60,28 @@ function App() {
             <ForConsumers />
           </Route>
           <Route exact path="/ForContractors">
-           <ForContractors />
+            <ForContractors />
           </Route>
         </Switch>
       </Router>
     );
   }
   if (isAuthenticated) {
-    return (
-      <div className="App">
-        <NavBarIn />
-        {/* <ConsumerProfile /> */}
-        {console.log(user)}
-      </div>
-    );
+    // console.log(user['https://example.com/roles']);
+    return <div>hello from login</div>;
+    // return (
+    //   <Router>
+    //     <Switch>
+    //       <Route exact path="/ConsumerHome">
+    //         {/* <ForContractors /> */}
+    //         <div>hello from correct path</div>
+    //         {console.log(user)}
+    //       </Route>
+    //     </Switch>
+    //   </Router>
+    // );
+    // if (user['https://example.com/roles'] === 'Contractor') {
+    // }
   }
 }
 
