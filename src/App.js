29111afb6e1b3ae267f1cfe7f1b1ba4d4be53804
Warren Williams <warrenw239@gmail.com>
2021-09-21 16:00:50 -0400

    [build] create routes and base pages for logged out status

--- src/App.js
+++ src/App.js
@@ -1,35 +1,44 @@
 import React from 'react';
 import './App.css';
 import NavBarOut from './components/NavBarOut';
-import { Switch, Router, Link } from 'react-router-dom';
+import { Switch, BrowserRouter as Router, Link, Route } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import NavBarIn from './components/NavBarIn';
+import LandingPage from './Pages/LandingPage';
 // import ConsumerProfile from './components/ConsumerProfile';
 
 function App() {
-    const { isAuthenticated, isLoading, user } = useAuth0();
+  const { isAuthenticated, isLoading, user } = useAuth0();
 
-
-    if (isLoading) {
-      return <div>loading....</div>;
-    }
-    if (!isAuthenticated) {
-        return (
-            <div className="App">
-                <NavBarOut />
-                
-            </div>
-        );
-    }
-    if (isAuthenticated) {
-        return (
-            <div className="App">
-                <NavBarIn />
-                {/* <ConsumerProfile /> */}
-                {console.log(user)}
-            </div>
-        );
-    }
+  if (isLoading) {
+    return <div>loading....</div>;
+  }
+  if (!isAuthenticated) {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route exact path="/ForConsumers">
+            <div>consumers</div>
+          </Route>
+          <Route exact path="/ForContractors">
+            <div>contractors</div>
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+  if (isAuthenticated) {
+    return (
+      <div className="App">
+        <NavBarIn />
+        {/* <ConsumerProfile /> */}
+        {console.log(user)}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Pages/LandingPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBarOut from '../components/NavBarOut';
+
+const LandingPage = () => {
+  return (
+    <div>
+      <NavBarOut />
+    </div>
+  );
+};
+
+export default LandingPage;
