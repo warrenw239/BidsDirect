a9c95a0a10b4b01157c5a09fb982b68cde1bde72
Warren Williams <warrenw239@gmail.com>
2021-09-22 16:34:20 -0400

    [configure] auth0 redirect on button click

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Auth0providerWithHistory from './auth0-provider-with-history';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+      <Auth0providerWithHistory>
+        <App />
+    </Auth0providerWithHistory>
+    </Router>,
+    document.getElementById('root'),
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
