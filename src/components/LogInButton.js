a1e50da6002c4104caa537917d6d38d2b35863c9
Warren Williams <warrenw239@gmail.com>
2021-09-22 16:16:42 -0400

    [build] create buttons for logging in and out

---
+++ src/components/LogInButton.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import '../styles/LogInButton.css';
+
+const LogInButton = () => {
+  return <Button></Button>;
+};
+
+export default LogInButton;
---
+++ src/components/LogOutButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+
+const LogOutButton = () => {
+  return <Button>Log Out</Button>;
+};
+
+export default LogOutButton;
