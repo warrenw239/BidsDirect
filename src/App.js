f5b1eba10f59094913e290c278180914576f1684
Warren Williams <warrenw239@gmail.com>
2021-09-22 17:23:26 -0400

    [fix] fix useAuth0 usage, and use its elements in the app page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
 import React from 'react';
 import './App.css';
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 import LandingPage from './pages/LandingPage';
-
+import LoggedInConsumer from './pages/LoggedInConsumer';
 
 function App() {
-  return <LandingPage />;
+  const { isAuthenticated, isLoading, user } = useAuth0();
+
+  if (isLoading) {
+    // setDisplay(<NavBarIn />);
+    return <div>loading 123...</div>;
+  }
+  if (!isAuthenticated) {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/" component={LandingPage}></Route>
+        </Switch>
+      </Router>
+    );
+  }
+  if (
+    isAuthenticated &&
+    user &&
+    user['https://example.com/roles'] === 'Consumer'
+  ) {
+    console.log(user);
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LoggedInConsumer />
+          </Route>
+          <Route exact path="/securedConsumer">
+            <div>secured page. add info about projects</div>
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/pages/LoggedInConsumer.js
+++ src/pages/LoggedInConsumer.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import NavBarConsumerHome from '../components/NavBarConsumerHome';
-import NavBarIn from '../components/NavBarIn';
+
 
 const LoggedInConsumer = () => {
   return (
