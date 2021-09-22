1786a931f97f5b21bd298ea429e6da6696186915
Warren Williams <warrenw239@gmail.com>
2021-09-22 17:03:35 -0400

    [build] create consumer navbar and consumer home page

---
+++ src/pages/LoggedInConsumer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavBarConsumerHome from '../components/NavBarConsumerHome';
+import NavBarIn from '../components/NavBarIn';
+
+const LoggedInConsumer = () => {
+  return (
+    <div>
+      <NavBarConsumerHome />
+    </div>
+  );
+};
+
+export default LoggedInConsumer;
