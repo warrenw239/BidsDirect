07247122530e98ca378fc00a46d444b7a7798645
Warren Williams <warrenw239@gmail.com>
2021-09-22 14:57:14 -0400

    hold current project to restart and make better tickets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-// import NavBarOut from './components/NavBarOut';
-// import {
-//   Switch,
-//   BrowserRouter as Router,
-//   Link,
-//   Route,
-//   Redirect,
-// } from 'react-router-dom';
-// import { useAuth0 } from '@auth0/auth0-react';
-// import NavBarIn from './components/NavBarIn';
-// import LandingPage from './Pages/LandingPage';
-// import ForConsumers from './Pages/ForConsumers';
-// import ForContractors from './Pages/ForContractors';
-// import LoggedInConsumer from './Pages/LoggedInConsumer';
-// import Loading from './Pages/Loading';
+import NavBarOut from './components/NavBarOut';
+import {
+  Switch,
+  BrowserRouter as Router,
+  Link,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
+import NavBarIn from './components/NavBarIn';
+import LandingPage from './Pages/LandingPage';
+import ForConsumers from './Pages/ForConsumers';
+import ForContractors from './Pages/ForContractors';
+import LoggedInConsumer from './Pages/LoggedInConsumer';
+import Loading from './Pages/Loading';
+import LoggedInContractor from './Pages/LoggedInContractor';
 
 function App() {
-  // const { isAuthenticated, isLoading, user } = useAuth0();
-  return <div>hello world</div>;
+  const { isAuthenticated, isLoading, user } = useAuth0();
+
+  if (isLoading) {
+    // setDisplay(<NavBarIn />);
+    return <Loading />;
+  }
+  if (!isAuthenticated) {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route exact path="/ForConsumers">
+            <ForConsumers />
+          </Route>
+          <Route exact path="/ForContractors">
+            <ForContractors />
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+  if (
+    isAuthenticated &&
+    user &&
+    user['https://example.com/roles'] === 'Consumer'
+  ) {
+    console.log(user);
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LoggedInConsumer />
+          </Route>
+          <Route exact path="/securedConsumer">
+            <div>secured page. add info about projects</div>
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
+  if (
+    isAuthenticated &&
+    user &&
+    user['https://example.com/roles'] === 'Contractor'
+  ) {
+    console.log(user);
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LoggedInContractor />
+          </Route>
+          <Route exact path="/securedConsumer">
+            <div>secured page. add info about projects</div>
+          </Route>
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
---
+++ src/Pages/LoggedInContractor.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBarInContractorHome from '../components/NavBarInContractorHome';
+
+const LoggedInContractor = () => {
+  return (
+    <div>
+      <NavBarInContractorHome />
+    </div>
+  );
+};
+
+export default LoggedInContractor;
