8413439c4a50920ac42ea7c0f33bce985eca359a
Warren Williams <warrenw239@gmail.com>
2021-09-22 19:24:32 -0400

    [build] create about contractors page

---
+++ src/pages/ForContractors.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavBarForAboutPages from '../components/NavBarForAboutPages';
+
+const ForContractors = () => {
+  return (
+    <div>
+      <NavBarForAboutPages />
+    </div>
+  );
+};
+
+export default ForContractors;
