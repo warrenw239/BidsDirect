b73ac347b75f6dc76661f71df6347398adf504e2
Warren Williams <warrenw239@gmail.com>
2021-09-22 16:54:57 -0400

    [fix] log in button to properly log out of user account and direct back to landing page

--- src/components/LogOutButton.js
+++ src/components/LogOutButton.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
 import { Button } from 'react-bootstrap';
 
 const LogOutButton = () => {
-  return <Button>Log Out</Button>;
+    const { logout } = useAuth0();
+
+    return (
+        <Button
+            onClick={() => logout()}
+            id="qsLogOutButton"
+            variant="danger"
+            className="btn-margin"
+        >
+            Log Out
+        </Button>
+    );
 };
 
 export default LogOutButton;
