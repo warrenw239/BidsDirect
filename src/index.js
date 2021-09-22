8c2c813466326ebd06e789650453c10d694728dd
Warren Williams <warrenw239@gmail.com>
2021-09-22 18:34:24 -0400

    [clean] remove remaining CRA unneeded files and imports

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-
 import Auth0providerWithHistory from './auth0-provider-with-history';
 
 ReactDOM.render(
