1922c4e03067fd0cc80d40b52efde0f2d91c005a
Warren Williams <warrenw239@gmail.com>
2021-09-22 16:44:59 -0400

    [build] create landing page and use it in the app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.css';
-import NavBarOut from './components/NavBarOut';
+import LandingPage from './pages/LandingPage';
+
 
 function App() {
-  return (
-    <div className="App">
-      <NavBarOut />
-    </div>
-  );
+  return <LandingPage />;
 }
 
 export default App;
