7f3f719ef9aed22b671d28222f34753b8b4a62b1
Warren Williams <warrenw239@gmail.com>
2021-09-22 16:00:32 -0400

    [build] NavBar for home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './App.css';
+import NavBarOut from './components/NavBarOut';
 
 function App() {
   return (
     <div className="App">
+      <NavBarOut />
     </div>
   );
 }
---
+++ src/components/NavBarOut.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import '../styles/NavBar.css';
+
+
+const NavBarOut = () => {
+  const [navStatus, useNavBar] = useState(false);
+  return (
+    <div>
+      <header className="header">
+        <div className="navContainer">
+          <span
+            className="logo"
+            style={{
+              color: '#fff',
+              fontStyle: 'italic',
+              fontWeight: '400',
+            }}
+          >
+            BidsDirect: Find The Best Fit For Your Next Project
+          </span>
+          <nav>
+
+            <ul
+              className="mainNav"
+              style={navStatus ? { transform: 'translateX(0)' } : null}
+            >
+              <li>
+                <a className="mainNavLink" href="/ForConsumers">
+                  For Consumers
+                </a>
+              </li>
+              <li>
+                <a className="mainNavLink" href="/ForContractors">
+                  For Contractors
+                </a>
+              </li>
+              <li></li>
+            </ul>
+          </nav>
+          <button
+            onClick={() => useNavBar((currentStatus) => !currentStatus)}
+            className={`navToggle ${navStatus ? 'open' : null}`}
+          >
+            <span />
+            <span />
+            <span />
+          </button>
+          <div
+            onClick={() => useNavBar((currentStatus) => !currentStatus)}
+            className={`overlay ${navStatus ? 'open' : ''}`}
+          />
+        </div>
+      </header>
+      <div className="wrapper"></div>
+    </div>
+  );
+};
+
+export default NavBarOut;
