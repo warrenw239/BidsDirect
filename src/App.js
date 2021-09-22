143852a06e5bdb8f5426b9165b337ffae05d3485
Warren Williams <warrenw239@gmail.com>
2021-09-22 12:13:48 -0400

    [test]

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import NavBarOut from './components/NavBarOut';
-import {
-  Switch,
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Redirect,
-} from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react';
-import NavBarIn from './components/NavBarIn';
-import LandingPage from './Pages/LandingPage';
-import ForConsumers from './Pages/ForConsumers';
-import ForContractors from './Pages/ForContractors';
-import LoggedInConsumer from './Pages/LoggedInConsumer';
-import Loading from './Pages/Loading';
+// import NavBarOut from './components/NavBarOut';
+// import {
+//   Switch,
+//   BrowserRouter as Router,
+//   Link,
+//   Route,
+//   Redirect,
+// } from 'react-router-dom';
+// import { useAuth0 } from '@auth0/auth0-react';
+// import NavBarIn from './components/NavBarIn';
+// import LandingPage from './Pages/LandingPage';
+// import ForConsumers from './Pages/ForConsumers';
+// import ForContractors from './Pages/ForContractors';
+// import LoggedInConsumer from './Pages/LoggedInConsumer';
+// import Loading from './Pages/Loading';
 
 function App() {
-  const { isAuthenticated, isLoading, user } = useAuth0();
+  // const { isAuthenticated, isLoading, user } = useAuth0();
+  return <div>hello world</div>;
 }
 
 export default App;
--- src/components/NavBarIn.js
+++ src/components/NavBarIn.js
@@ -3,70 +3,62 @@ import '../styles/NavBar.css';
 import LogOutButton from './LogOutButton';
 
 const NavBarIn = () => {
-    const [navStatus, useNavBar] = useState(false);
-    return (
-        <div>
-            <header className="header">
-                <div className="navContainer">
-                    <span
-                        className="logo"
-                        style={{
-                            color: '#fff',
-                            fontStyle: 'italic',
-                            fontWeight: '400',
-                        }}
-                    >
-                        BidsDirect: Find The Best Fit For Your Next Project
-                    </span>
-                    <nav>
-                        <LogOutButton />
-                        <ul
-                            className="mainNav"
-                            style={
-                                navStatus
-                                    ? { transform: 'translateX(0)' }
-                                    : null
-                            }
-                        >
-                            <li>
-                                <a className="mainNavLink" href="/securedConsumer">
-                                    Current Projects
-                                </a>
-                            </li>
-                            <li>
-                                <a className="mainNavLink" href="/startNewProject">
-                                    Start New Project
-                                </a>
-                            </li>
-                            <li>
-                                <a className="mainNavLink" href="/ForConsumers">
-                                    Find Contractors
-                                </a>
-                            </li>
-                            <li></li>
-                        </ul>
-                    </nav>
-                    <button
-                        onClick={() =>
-                            useNavBar((currentStatus) => !currentStatus)
-                        }
-                        className={`navToggle ${navStatus ? 'open' : null}`}
-                    >
-                        <span />
-                        <span />
-                        <span />
-                    </button>
-                    <div
-                        onClick={() =>
-                            useNavBar((currentStatus) => !currentStatus)
-                        }
-                        className={`overlay ${navStatus ? 'open' : ''}`}
-                    />
-                </div>
-            </header>
-            <div className="wrapper"></div>
+  const [navStatus, useNavBar] = useState(false);
+  return (
+    <div>
+      <header className="header">
+        <div className="navContainer">
+          <span
+            className="logo"
+            style={{
+              color: '#fff',
+              fontStyle: 'italic',
+              fontWeight: '400',
+            }}
+          >
+            BidsDirect: Find The Best Fit For Your Next Project
+          </span>
+          <nav>
+            <LogOutButton />
+            <ul
+              className="mainNav"
+              style={navStatus ? { transform: 'translateX(0)' } : null}
+            >
+              <li>
+                <a className="mainNavLink" href="/securedConsumer">
+                  Current Projects
+                </a>
+              </li>
+              <li>
+                <a className="mainNavLink" href="/startNewProject">
+                  Start New Project
+                </a>
+              </li>
+              <li>
+                <a className="mainNavLink" href="/ForConsumers">
+                  Find Contractors
+                </a>
+              </li>
+              <li></li>
+            </ul>
+          </nav>
+          <button
+            onClick={() => useNavBar((currentStatus) => !currentStatus)}
+            className={`navToggle ${navStatus ? 'open' : null}`}
+          >
+            <span />
+            <span />
+            <span />
+          </button>
+          <div
+            onClick={() => useNavBar((currentStatus) => !currentStatus)}
+            className={`overlay ${navStatus ? 'open' : ''}`}
+          />
         </div>
-    );
+      </header>
+      <div className="wrapper"></div>
+    </div>
+  );
 };
 
 export default NavBarIn;
