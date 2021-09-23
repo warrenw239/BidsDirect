a9f3e9ca9343f904f97404e5a5651d9ae97e730d
Warren Williams <warrenw239@gmail.com>
2021-09-23 15:58:01 -0400

    [configure] routes to and from about page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import ForConsumers from './pages/ForConsumers';
 import LandingPage from './pages/LandingPage';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Switch>
       <Route exact path="/" component={LandingPage}></Route>
+      <Route exact path="/ForConsumers" component={ForConsumers}></Route>
       </Switch>
     </Router>
   );
--- src/pages/ForConsumers.js
+++ src/pages/ForConsumers.js
@@ -1,12 +1,12 @@
 
 import React from 'react';
-// import NavBarForAboutPages from '../components/NavBarForAboutPages';
+import NavBarForAboutPages from '../components/NavBarForAboutPages';
 import '../styles/NavBar.css';
 
 const ForConsumers = () => {
   return (
     <div>
-      {/* <NavBarForAboutPages /> */}
+      <NavBarForAboutPages />
     </div>
   );
 };
