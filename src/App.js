ec45f768181827fcc839fea83e1139791a55073c
Warren Williams <warrenw239@gmail.com>
2021-09-23 14:27:57 -0400

    [style] add style for navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import NavBarOut from './components/NavBarOut';
 
 function App() {
   return (
     <div className="App">
-      react
+      <NavBarOut />
     </div>
   );
 }
--- src/components/NavBarOut.js
+++ src/components/NavBarOut.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import '../styles/NavBar.css';
 
 
 const NavBarOut = () => {
