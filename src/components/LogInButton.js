dd4d4db8ce65bcaa750ea95c3cfe8ff163cecc64
warrenw239 <84738630+warrenw239@users.noreply.github.com>
2021-09-23 19:45:44 -0400

    [build] log in button and styling

---
+++ src/components/LogInButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import '../styles/LogInButton.css';
+
+const LoginButton = () => <Button>temp</Button>;
+
+export default LoginButton;
