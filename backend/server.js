df21542256826ef0ab4acc5371a8fefc364d5288
Warren Williams <warrenw239@gmail.com>
2021-09-22 21:28:23 -0400

    [fix] clean up server file.

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,6 @@ const PORT = process.env.PORT || 9000;
 app.use(express.static('/Users/warren/Documents/Project/BidsDirect/build'));
 
 app.get('/', function (req, res) {
-    // console.log(PORTENV, 'port');
-//   res.sendFile(path.join('BidsDirect', 'build', 'index.html'));
   res.sendFile('/Users/warren/Documents/Project/BidsDirect/build');
 });
 
