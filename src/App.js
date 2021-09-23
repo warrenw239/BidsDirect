edc3d7f9dc7dd34a2467c354d6eacff92101c4ce
warrenw239 <84738630+warrenw239@users.noreply.github.com>
2021-09-23 19:10:32 -0400

    [fix] errors

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 
-function App() {
-  return ( 
-    <Router>
-      <Switch>
-      <Route exact path="/" component={LandingPage}></Route>
-      </Switch>
-    </Router>
-  );
-}
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={LandingPage}></Route>
+            </Switch>
+        </Router>
+    );
+};
 
 export default App;
