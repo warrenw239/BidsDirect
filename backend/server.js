b3223b627279a8a466035be21ec85e57ae94b5ab
Warren Williams <warrenw239@gmail.com>
2021-09-22 23:13:20 -0400

    [hold to reorganize repo]

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,8 @@ const app = express();
 const PORT = process.env.PORT || 9000;
 app.use(express.static('/Users/warren/Documents/Project/BidsDirect/build'));
 
-app.get('/', function (req, res) {
-  res.sendFile('/Users/warren/Documents/Project/BidsDirect/build');
+app.get('/*', function (req, res) {
+  res.sendFile('/Users/warren/Documents/Project/BidsDirect/build/index.html');
 });
 
 app.listen(PORT, () => {
