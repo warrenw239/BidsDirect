f04f6d9263f5f5627684f71182696367c8b59dac
Warren Williams <warrenw239@gmail.com>
2021-09-23 16:18:55 -0400

    [build] create page and add route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, BrowserRouter as Router, Route } from 'react-router-dom';
+import ForContractors from './pages/ForContractors';
 import LandingPage from './pages/LandingPage';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
     <Router>
       <Switch>
       <Route exact path="/" component={LandingPage}></Route>
+      <Route exact path="/ForContractors" component={ForContractors}></Route>
       </Switch>
     </Router>
   );
---
+++ src/pages/ForContractors.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import NavBarForAboutPages from '../components/NavBarForAboutPages';
+
+const ForContractors = () => {
+  return (
+    <div>
+      <NavBarForAboutPages />
+    </div>
+  );
+};
+
+export default ForContractors;
