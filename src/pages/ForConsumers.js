528a547c6b60c6484753964dd1de2c247c085d55
Warren Williams <warrenw239@gmail.com>
2021-09-23 15:50:44 -0400

    [build] page for about consumers info

---
+++ src/pages/ForConsumers.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+// import NavBarForAboutPages from '../components/NavBarForAboutPages';
+import '../styles/NavBar.css';
+
+const ForConsumers = () => {
+  return (
+    <div>
+      {/* <NavBarForAboutPages /> */}
+    </div>
+  );
+};
+
+export default ForConsumers;
